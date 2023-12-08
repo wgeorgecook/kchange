9aca682356a8227b26df9e0bce5e275a182d82a8
William Cook <wcook@conquer.io>
2023-12-08 15:19:23 -0500

    refactor to genericise outputs

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -10,9 +9,19 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-type kobject int
+type ktype int
 
-func (k kobject) String() string {
+const (
+	deployment ktype = iota
+	cronjob
+)
+
+type kobject struct {
+	kind ktype
+	name string
+}
+
+func (k ktype) String() string {
 	switch k {
 	case deployment:
 		return "deployment"
@@ -23,26 +32,32 @@ func (k kobject) String() string {
 	}
 }
 
-const (
-	deployment kobject = iota
-	cronjob
-)
+func addToRequests(k ktype, s []string, r []kobject) []kobject {
+	var ks []kobject
+	for _, t := range s {
+		ko := kobject{kind: k, name: t}
+		ks = append(ks, ko)
+	}
 
-func printChangeCause(o kobject, r []string) error {
-	for _, request := range r {
-		out, err := exec.Command("kubectl", "get", o.String(), request, "-o", "jsonpath={.metadata.annotations.kubernetes\\.io/change-cause}").Output()
+	r = append(r, ks...)
+	return r
+}
+
+func printChangeCause(r []kobject) error {
+	for _, o := range r {
+		out, err := exec.Command("kubectl", "get", o.kind.String(), o.name, "-o", "jsonpath={.metadata.annotations.kubernetes\\.io/change-cause}").Output()
 		if err != nil {
-			return err
+			return fmt.Errorf("cannot print %s %s change cause: %v", o.kind.String(), o.name, err)
 		}
 		fmt.Println(string(out))
 	}
 	return nil
 }
+
 func main() {
 	var d *cli.StringSlice
 	var j *cli.StringSlice
-	var deployments []string
-	var cronjobs []string
+	var requests []kobject
 	app := &cli.App{
 		Name:  "Kubernetes Change Cause",
 		Usage: "Prints the kubernetes.io/change-cause for the provided deployment",
@@ -54,7 +69,7 @@ func main() {
 				Destination: d,
 				Required:    false,
 				Action: func(ctx *cli.Context, s []string) error {
-					deployments = append(deployments, s...)
+					requests = addToRequests(deployment, s, requests)
 					return nil
 				},
 			},
@@ -65,20 +80,14 @@ func main() {
 				Destination: j,
 				Required:    false,
 				Action: func(ctx *cli.Context, s []string) error {
-					cronjobs = append(cronjobs, s...)
+					requests = addToRequests(cronjob, s, requests)
 					return nil
 				},
 			},
 		},
 		Action: func(ctx *cli.Context) error {
-			if deployments == nil && cronjobs == nil {
-				return errors.New("must provide at least one kubernetes object to request change cause")
-			}
-			if err := printChangeCause(deployment, deployments); err != nil {
-				log.Printf("cannot print deployment change cause: %v", err)
-			}
-			if err := printChangeCause(cronjob, cronjobs); err != nil {
-				log.Printf("cannot print cronjob change cause: %v", err)
+			if err := printChangeCause(requests); err != nil {
+				log.Println(err)
 			}
 
 			return nil
